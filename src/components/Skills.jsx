787ebfdde50af8706ebd47b4420f907e55002ff8
francisluy <clydeluy12@gmail.com>
2024-03-12 21:40:53 +0400

    updated Skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,33 @@
+import { useState, useEffect } from "react";
+import { db } from "../config/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
 export default function Skills() {
+  const [skillsData, setSkillsData] = useState([
+    {
+      title: "Loading...",
+      list: ["my skill"],
+      id: "1",
+    },
+  ]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const skills = collection(db, "skills");
+      try {
+        const data = await getDocs(skills);
+        const dataObject = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setSkillsData(dataObject);
+      } catch (error) {
+        alert(error);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div
       id="skills"
@@ -6,38 +35,21 @@ export default function Skills() {
     >
       <div className="flex w-full max-w-[1280px] flex-col items-center pb-8">
         <h2 className="py-8 text-2xl font-bold">My Skills</h2>
-        <div className="grid w-full  justify-items-center gap-6  py-4  md:grid-cols-2 lg:grid-cols-4">
-          <div className="image-shadow flex h-[150px] w-full flex-col items-center justify-center  rounded-xl bg-white">
-            <p className="font-semibold">IoT Development</p>
-            <ul className=" list-inside list-disc">
-              <li>Arduino</li>
-              <li>Raspberry Pi</li>
-            </ul>
-          </div>
-          <div className="image-shadow flex h-[150px] w-full flex-col items-center justify-center rounded-xl bg-white">
-            <p className="font-semibold">UI/Ux Design</p>
-            <ul className=" list-inside list-disc">
-              <li>Canva</li>
-              <li>Photoshop</li>
-            </ul>
-          </div>
-          <div className="image-shadow flex h-[150px] w-full flex-col items-center justify-center rounded-xl bg-white">
-            <p className="font-semibold">Programming</p>
-            <ul className=" list-inside list-disc">
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Javascript</li>
-              <li>C++</li>
-            </ul>
-          </div>
-          <div className="image-shadow flex h-[150px] w-full flex-col items-center justify-center rounded-xl bg-white">
-            <p className="font-semibold">Soft Skills</p>
-            <ul className="list-inside list-disc">
-              <li>Leadership</li>
-              <li>Motivator</li>
-              <li>Team Player</li>
-            </ul>
-          </div>
+
+        <div className="grid w-full  justify-items-center gap-6 py-4 md:grid-cols-2 lg:grid-cols-4">
+          {skillsData.map((skill) => (
+            <div
+              key={skill?.id}
+              className="image-shadow flex min-h-[150px] w-full grow flex-col items-center justify-center rounded-xl bg-white py-10"
+            >
+              <p className="font-semibold">{skill.title}</p>
+              <ul className=" list-inside list-disc">
+                {skill.list.map((item) => (
+                  <li key={item}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
     </div>
